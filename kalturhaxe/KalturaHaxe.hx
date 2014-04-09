e982effd76ef919a851cd8575ca83302efc4eedc
JbIPS <jeanbaptiste.richardet@gmail.com>
2014-04-09 16:48:15 +0200

    init with KS

--- kalturhaxe/KalturaHaxe.hx
+++ kalturhaxe/KalturaHaxe.hx
@@ -11,18 +11,20 @@ class KalturaHaxe{
 	var partnerId: Int;
 	var client: KalturaClient;
 
-	public function new(partnerId:Int, ?serviceUrl: String = "http://www.kaltura.com/"){
+	public function new(?partnerId:Int, ?serviceUrl: String = "http://www.kaltura.com/", ?ks: String){
 		this.partnerId = partnerId;
 		var config = new KalturaConfiguration(partnerId);
 		config.serviceUrl = serviceUrl;
 		client = new KalturaClient(config);
+		if(ks != null)
+			client.ks = ks;
 	}
 
 	///
 	// API
 	//
 
-	public function createConnection(secret:String, ?userId: String, ?sessionType:KalturaSessionType, ?expiry: Int = 86400, ?privileges: String, callback: Bool -> String -> Void):Void
+	public function createConnection(secret:String, ?userId: String = "", ?sessionType:KalturaSessionType, ?expiry: Int = 86400, ?privileges: String, callback: Bool -> String -> Void):Void
 	{
 		var type = sessionType != null ? sessionType : KalturaSessionType.USER;
 
@@ -36,13 +38,16 @@ class KalturaHaxe{
 	public function getBitrateWiseAsset(entryId:String, bitrate: Int, ?storageId: String, ?forceProxy: Bool = false, callback:Bool -> String -> Void):Void
 	{
 		client.flavorAsset.getEntryById(function(success, result){
+			//trace("callback1: ", success);
 			if(!success){
-				js.Lib.alert(result);
+				//js.Lib.alert(client.ks+", "+entryId+", "+result);
 				callback(success, result);
 			}
 			var res = Json.parse(result);
+			//trace(res);
 			var a = new Array<FlavorAssetResponse>();
 			for(f in Reflect.fields(res))
+				//trace(f);
 				a.push(KalturaResponseTypes.createFlavorAssetResponse(Reflect.field(res, f)));
 
 			var asset = selectByBitrate(a, bitrate);
@@ -66,7 +71,7 @@ class KalturaHaxe{
 		});
 
 		var i = 0;
-		while(i < candidates.length && Std.parseInt(candidates[i].bitrate) > targetedBitrate)
+		while(i < candidates.length && candidates[i].bitrate > targetedBitrate)
 			i++;
 
 		if(i == candidates.length)
@@ -75,7 +80,7 @@ class KalturaHaxe{
 			return candidates[i];
 	}
 
-/*
+	/*
 client.ks = results.substr(1, results.length-2);
 				client.flavorAsset.getEntryById(function(success, results){
 					if(!success)
